89441d3e85b0deba94db67a1568eaaabc561e0dc
vinzzz001 <vinzzz001@gmail.com>
2017-10-16 12:55:28 +0200

    NewTestCar
    Made a new test scene and sorted some things out

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    [SerializeField]
+    int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	
+	void FixedUpdate () {
+        //Testing movement
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Vector3 movement = new Vector3(speed, 0, 0 );
+            rb.velocity = movement;
+        }
+    }
+}
